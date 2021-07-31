f2abde4cac4fd68575665f99f4d26eefa19a6653
EdiTV2021 <88168021+EdiTV2021@users.noreply.github.com>
2021-07-30 19:01:40 -0500

    Add files via upload

---
+++ 03_personal_info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 18:40:38 2021
+
+@author: Edison
+"""
+
+nombre= input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+loc= input("Ingresa tu localización: ")
+edad = input("Ingres tu edad: ")
+space= " "
+print("Nombre:", nombre + space + apellido + 
+      space + "Localización: ", loc + space + "Edad: ",edad)

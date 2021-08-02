61e87bfe58d646fa3413262974b7c5abe8c2f6d6
EdiTV2021 <88168021+EdiTV2021@users.noreply.github.com>
2021-08-02 18:37:09 -0500

    Add files via upload

---
+++ for_ejercico.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 19:55:35 2021
+
+@author: Edison
+"""
+print("Inicio")
+lista=["R1","R2","R3","R4"]
+# print(lsita[0])
+# print(lsita[1])
+# print(lsita[2])
+# print(lsita[3])
+for item in lista:
+    print(item)
+    print("IN for")
+print("Fin")

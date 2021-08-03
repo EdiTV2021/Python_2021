0034679a412b9b19ae3f8c9731d7985841b8437a
EdiTV2021 <88168021+EdiTV2021@users.noreply.github.com>
2021-08-03 18:36:36 -0500

    Add files via upload

---
+++ funcion_resta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 20:37:21 2021
+
+@author: Edison
+"""
+
+def resta(a,b):
+    print("La resta entre: ",a,"-",b,"es igual a",a-b,"\n")
+    
+resta(5,2)
+resta(2,5)     
+resta(a=2,b=5)
+resta(b=5,a=2)   
+resta(5,b=2)
---
+++ funcion_return.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 18:29:35 2021
+
+@author: Edison
+"""
+
+#funciones con retorno
+def uno(a,b):
+    return(a*b)
+print(uno(5,2))
+w=uno(5,2)
+suma=w+5
+print(suma)

3750719f00b5308a1f02de5c98affa4c76395eb9
EdiTV2021 <88168021+EdiTV2021@users.noreply.github.com>
2021-08-03 19:38:10 -0500

    Add files via upload

---
+++ funcion_para_crear_lista.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 19:34:21 2021
+
+@author: Edison
+"""
+#funcion para crear listas
+def crealista(n):
+    lista=[]
+    for i in range (n):
+        lista.append(i)
+    return lista
+print(crealista(15))
+resultado=crealista(20)
